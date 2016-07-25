605f601d9aa8edc5def479536cf5668f752c1ead
Darren Vong <d.vongy@gmail.com>
2016-07-25 03:37:48 +0100

    Added (two!) solutions to the 5th question of the extra problem

--- MovieRatingExercise/MovieRating.sql
+++ MovieRatingExercise/MovieRating.sql
@@ -136,3 +136,21 @@ WHERE mID NOT IN (SELECT mID
                         WHERE name = "Chris Jackson") AS cjID
                     JOIN Rating USING(rID));
 
+
+/* For all pairs of reviewers such that both reviewers gave a rating to the same
+movie, return the names of both reviewers. Eliminate duplicates, don't pair
+reviewers with themselves, and include each pair only once. For each pair,
+return the names in the pair in alphabetical order. */
+
+/* Method 1 */
+SELECT DISTINCT R1.name, R2.name
+FROM (Reviewer JOIN Rating USING(rID)) AS R1,
+  (Reviewer JOIN Rating USING(rID)) AS R2
+WHERE R1.name < R2.name AND R1.mID = R2.mID
+ORDER BY R1.name, R2.name;
+
+/* (Slightly terser) Method 2 */
+SELECT DISTINCT R1.name, R2.name
+FROM (Rating JOIN Reviewer USING(rID)) AS R1
+  JOIN (Rating JOIN Reviewer USING(rID)) AS R2
+ON R1.mID = R2.mID AND R1.name < R2.name;
